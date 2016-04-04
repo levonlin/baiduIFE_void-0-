52d071616e566e38904089d3d861e36cb926d676
Levon <levonlin1@gmail.com>
2016-04-04 17:30:21 +0800

    add task18 from levon

---
+++ stage2/task18/task18_levon/task18.js
@@ -0,0 +1,71 @@
+var queue = [10, 3, 7, 12, 17, 30];
+
+function render() {
+	var displayArea = document.getElementById('display');
+	displayArea.innerHTML = '';
+	for (var i in queue) {
+		var newItem = document.createElement('span');
+		newItem.textContent = queue[i];
+		newItem.className = 'num-element';
+		displayArea.appendChild(newItem);
+	}
+}
+
+function isNumberString(str) {
+	if (/^\d+$/.test(str)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function delegateClickEvent() {
+	var container = document.getElementById('container');
+	container.addEventListener('click', function (event) {
+		var target = event.target;
+		var inputText = document.getElementsByClassName('input-text')[0].value;
+		switch (target.className) {
+			case 'left-in-btn':
+			if (isNumberString(inputText)) {
+				queue.unshift(inputText); 
+			}else {
+				alert('请输入数字');
+			} 
+			render();
+			break;
+
+			case 'right-in-btn':
+			if (isNumberString(inputText)) {
+				queue.push(inputText); 
+			}else {
+				alert('请输入数字');
+			} 
+			render();
+			break;
+
+			case 'left-out-btn':
+			alert(queue.shift());
+			render();
+			break;
+
+			case 'right-out-btn':
+			alert(queue.pop());
+			render();
+			break;	
+
+			case 'num-element':
+			var numElements = document.getElementById('display').children;
+			var index = Array.prototype.indexOf.call(numElements, target);
+			queue.splice(index, 1);
+			render();
+			break;		
+		}
+	}, false);
+}
+
+function init() {
+	delegateClickEvent();
+	render();
+}
+
+init();
