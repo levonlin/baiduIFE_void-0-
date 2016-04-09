4ea41e24102ef3f72bb936d3d54d6dfbef0a4d20
Levon <levonlin1@gmail.com>
2016-04-09 21:28:49 +0800

    levon readd his task21 code

--- stage2/task21/task21_levon/task21.js
+++ stage2/task21/task21_levon/task21.js
@@ -1,27 +1,91 @@
 /**
- * 定义一个对象工厂
+ * 定义构造函数
  */
 function InputBox(container) {
 	var queue = [];
-	var displayArea = container.getElementsByClassName('display')[0];
+	this.displayArea = container.getElementsByClassName('display')[0];
 
+	/**
+	 * 在容器加个点击事件代理，对点击事件进行分发
+	 */
+	this.delegateClickEvent = function () {
+		var that = this;
+		container.addEventListener('click', function (event) {
+			var target = event.target;
+			var textBox = container.getElementsByClassName('input-text')[0];
+			var inputText = textBox.value;
+			switch (target.className) {
+				case 'left-in-btn':
+				queue = that.textLeftIn(queue, inputText, 10);
+				textBox.value = '';
+				that.render(queue);
+				break;	
+
+				case 'display-item':
+				that.deleteItem(queue, target);
+				that.render(queue);
+				break;
+
+				case 'delete-info':
+				that.deleteItem(queue, target.parentNode);
+				that.render(queue);
+				break;	
+			}
+		}, false);
+	};
+
+	/**
+	 * 在容器加个键盘事件代理，对键盘事件进行分发
+	 */
+	this.delegateKeyBoardEvent = function () {
+		var that = this;
+		container.addEventListener('keyup', function (event) {
+			var target = event.target;
+			var textBox = container.getElementsByClassName('input-text')[0];
+			var inputText = textBox.value;
+			switch (target.className) {
+				case 'input-text':
+				if (event.keyCode === 13 || /[，,\s]/.test(inputText)) {
+					queue = that.textLeftIn(queue, inputText, 10);
+					textBox.value = '';
+					that.render(queue);
+				}
+				break;
+			}
+		}, false);
+	};
+
+	/**
+	/* 对象初始化函数
+ 	*/
+	this.init = function (isListenKeyBoard) {
+		this.delegateClickEvent();
+		if (isListenKeyBoard) {
+			this.delegateKeyBoardEvent();
+		}
+		this.render(queue);
+	};
+}
+
+// 将公有方法放到原先对象里，避免重复定义
+InputBox.prototype = {
 	/**
 	 * 渲染dom
 	 */
-	function render(arr) {
-		displayArea.innerHTML = '';
+	render: function (arr) {
+		this.displayArea.innerHTML = '';
 		for (var i in arr) {
 			var newItem = document.createElement('span');
 			newItem.innerHTML = '<span class="delete-info">点击删除&nbsp;</span>' + arr[i];
 			newItem.className = 'display-item';
-			displayArea.appendChild(newItem);
+			this.displayArea.appendChild(newItem);
 		}
-	}
+	},
 
 	/**
 	 * 对字符串中的html字符进行转义
 	 */
-	function htmlEscape(text) {
+	htmlEscape: function (text) {
 		return text.replace(/[<>"&]/g, function (match, pos, originString) {
 			switch (match) {
 				case '<':
@@ -34,12 +98,12 @@ function InputBox(container) {
 				return '&quot;';
 			}
 		});
-	}
+	},
 
 	/**
 	 * 数组去空字符元素操作
 	 */
-	function trimArray(arr) {
+	trimArray: function (arr) {
 		  var newArr = [];
 		  for (var i in arr) {
 		    if (arr[i] !== '') {
@@ -47,22 +111,22 @@ function InputBox(container) {
 		    }
 		  }
 		  return newArr;
-	}
+	},
 
 	/**
 	 * 输入的字符串按分隔符转为数组
 	 */
-	function formatInput(inputText) {
-		inputText = htmlEscape(inputText);
-		return trimArray(inputText.split(/[,，、\s\n]/));
-	}
+	formatInput: function (inputText) {
+		inputText = this.htmlEscape(inputText);
+		return this.trimArray(inputText.split(/[,，、\s\n]/));
+	},
 
 	/**
 	 * 输入的字符串按分隔符转为数组
 	 */
-	function textLeftIn(inputText, maxLength) {
+	textLeftIn: function (oldQueue,inputText, maxLength) {
 		var result = [];
-		var arr = formatInput(inputText).concat(queue);
+		var arr = this.formatInput(inputText).concat(oldQueue);
 		for (var i = arr.length - 1; i >= 0; i--) {
 			if (result.indexOf(arr[i]) === -1) {
 				result.unshift(arr[i]);
@@ -73,72 +137,17 @@ function InputBox(container) {
 		} else {
 			return result.slice(0, maxLength);
 		}
-	}
+	},
 
 	/**
 	 * 由dom元素删除数组中与其对应的元素
 	 */
-	function deleteItem(target) {
-		var displayItems = displayArea.children;
+	deleteItem: function (oldQueue, target) {
+		var displayItems = this.displayArea.children;
 		var index = Array.prototype.indexOf.call(displayItems, target);
-		queue.splice(index, 1);
+		oldQueue.splice(index, 1);
 	}
-
-	/**
-	 * 在容器加个点击事件代理，对点击事件进行分发
-	 */
-	function delegateClickEvent() {
-		container.addEventListener('click', function (event) {
-			var target = event.target;
-			var inputText = container.getElementsByClassName('input-text')[0].value;
-			switch (target.className) {
-				case 'left-in-btn':
-				queue = textLeftIn(inputText, 10);
-				render(queue);
-				break;	
-
-				case 'display-item':
-				deleteItem(target);
-				render(queue);
-				break;
-
-				case 'delete-info':
-				deleteItem(target.parentNode);
-				render(queue);
-				break;	
-			}
-		}, false);
-	}
-
-	/**
-	 * 在容器加个键盘事件代理，对键盘事件进行分发
-	 */
-	function delegateKeyBoardEvent() {
-		container.addEventListener('keyup', function (event) {
-			var target = event.target;
-			switch (target.className) {
-				case 'input-text':
-				if (event.keyCode === 13 || event.keyCode === 188 || event.keyCode === 32) {
-					var inputText = container.getElementsByClassName('input-text')[0].value;
-					queue = textLeftIn(inputText, 10);
-					render(queue);
-				}
-				break;		
-			}
-		}, false);
-	}
-
-	/**
-	/* 对象初始化函数
- 	*/
-	this.init = function (isListenKeyBoard) {
-		delegateClickEvent();
-		if (isListenKeyBoard) {
-			delegateKeyBoardEvent();
-		}
-		render(queue);
-	};
-}
+};
 
 // 对象的实例化
 var tagsContainer = document.getElementsByClassName('tags-container')[0];
@@ -149,3 +158,4 @@ tagsInputBox.init(true);
 
 hobbiesInputBox = new InputBox(hobbiesContainer);
 hobbiesInputBox.init();
+
